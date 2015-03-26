b610962e09e949f2716002724536514a50ab69fc
YG <Yann@YGs-MacBook-Air.local>
2015-03-26 21:25:44 +0100

    Changed cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures mpg'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'mysql', '~> 6.0'
--- recipes/install_mysql.rb
+++ recipes/install_mysql.rb
@@ -6,6 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
 mysql_service 'default' do
   port '3306'
   version '5.5'
